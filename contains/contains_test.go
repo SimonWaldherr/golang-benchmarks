d738174bf808c64730f4540778c108f89411d8d5
Simon Waldherr <git@simon.waldherr.eu>
2017-04-30 00:57:54 +0200

    init

---
+++ contains/contains_test.go
@@ -0,0 +1,136 @@
+package contains
+
+import (
+	"bytes"
+	"regexp"
+	"strings"
+	"testing"
+)
+
+// strings.Contains
+func contains() bool {
+	return strings.Contains("Lorem Ipsum", "em Ip")
+}
+
+func containsNot() bool {
+	return strings.Contains("Lorem Ipsum", "Dolor")
+}
+
+func TestContains(t *testing.T) {
+	if contains() == false {
+		t.Error("ERROR: contains")
+	}
+	if containsNot() == true {
+		t.Error("ERROR: contains not")
+	}
+}
+
+func BenchmarkContains(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		contains()
+	}
+}
+
+func BenchmarkContainsNot(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		containsNot()
+	}
+}
+
+// bytes.Contains
+func containsBytes() bool {
+	return bytes.Contains([]byte("Lorem Ipsum"), []byte("em Ip"))
+}
+
+func containsBytesNot() bool {
+	return bytes.Contains([]byte("Lorem Ipsum"), []byte("Dolor"))
+}
+
+func TestContainsBytes(t *testing.T) {
+	if containsBytes() == false {
+		t.Error("ERROR: bytes contains")
+	}
+	if containsBytesNot() == true {
+		t.Error("ERROR: bytes contains not")
+	}
+}
+
+func BenchmarkContainsBytes(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		containsBytes()
+	}
+}
+
+func BenchmarkContainsBytesNot(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		containsBytesNot()
+	}
+}
+
+// regexp.MustCompile + regexp.MatchString
+func compileMatch(re *regexp.Regexp) bool {
+	matched := re.MatchString("Lorem Ipsum")
+	return matched
+}
+
+func compileMatchNot(re *regexp.Regexp) bool {
+	matched := re.MatchString("Lorem Ipsum")
+	return matched
+}
+
+func TestCompileMatch(t *testing.T) {
+	re1 := regexp.MustCompile("em Ip")
+	re2 := regexp.MustCompile("Dolor")
+	if compileMatch(re1) == false {
+		t.Error("ERROR: compile match")
+	}
+	if compileMatchNot(re2) == true {
+		t.Error("ERROR: compile match not")
+	}
+}
+
+func BenchmarkCompileMatch(b *testing.B) {
+	re := regexp.MustCompile("em Ip")
+	for n := 0; n < b.N; n++ {
+		compileMatch(re)
+	}
+}
+
+func BenchmarkCompileMatchNot(b *testing.B) {
+	re := regexp.MustCompile("Dolor")
+	for n := 0; n < b.N; n++ {
+		compileMatchNot(re)
+	}
+}
+
+// regexp.MatchString
+func match() bool {
+	matched, _ := regexp.MatchString("em Ip", "Lorem Ipsum")
+	return matched
+}
+
+func matchNot() bool {
+	matched, _ := regexp.MatchString("Dolor", "Lorem Ipsum")
+	return matched
+}
+
+func TestMatch(t *testing.T) {
+	if match() == false {
+		t.Error("ERROR: match")
+	}
+	if matchNot() == true {
+		t.Error("ERROR: match not")
+	}
+}
+
+func BenchmarkMatch(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		match()
+	}
+}
+
+func BenchmarkMatchNot(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		matchNot()
+	}
+}
---
+++ foreach/foreach_test.go
@@ -0,0 +1,74 @@
+package foreach
+
+import (
+	"testing"
+)
+
+var amap map[int]string
+var aslice []string
+
+func init() {
+	amap = map[int]string{
+		0: "lorem",
+		1: "ipsum",
+		2: "dolor",
+		3: "sit",
+		4: "amet",
+	}
+
+	aslice = []string{
+		"lorem",
+		"ipsum",
+		"dolor",
+		"sit",
+		"amet",
+	}
+}
+
+func forMap() {
+	for i := 0; i < len(amap); i++ {
+		_ = amap[i]
+	}
+}
+
+func rangeMap() {
+	for _, v := range amap {
+		_ = v
+	}
+}
+
+func rangeSlice() {
+	for _, v := range aslice {
+		_ = v
+	}
+}
+
+func rangeSliceKey() {
+	for k := range aslice {
+		_ = aslice[k]
+	}
+}
+
+func BenchmarkForMap(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		forMap()
+	}
+}
+
+func BenchmarkRangeMap(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		rangeMap()
+	}
+}
+
+func BenchmarkRangeSlice(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		rangeSlice()
+	}
+}
+
+func BenchmarkRangeSliceKey(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		rangeSliceKey()
+	}
+}
