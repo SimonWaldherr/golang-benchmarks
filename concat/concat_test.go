aba68d27732d47fde51ee7b3e72a128c8e663187
Simon Waldherr <git@simon.waldherr.eu>
2018-10-23 23:55:14 +0200

    add concat and foreach benchmarks

---
+++ concat/concat_test.go
@@ -0,0 +1,29 @@
+package concat
+
+import (
+	"bytes"
+	"strings"
+	"testing"
+)
+
+func BenchmarkConcatString(b *testing.B) {
+	var str string
+	for n := 0; n < b.N; n++ {
+		str += "x"
+	}
+}
+
+func BenchmarkConcatBuffer(b *testing.B) {
+	var buffer bytes.Buffer
+	for n := 0; n < b.N; n++ {
+		buffer.WriteString("x")
+
+	}
+}
+
+func BenchmarkConcatBuilder(b *testing.B) {
+	var builder strings.Builder
+	for n := 0; n < b.N; n++ {
+		builder.WriteString("x")
+	}
+}
---
+++ regexp/regexp_test.go
@@ -0,0 +1,29 @@
+package regexp
+
+import (
+	"regexp"
+	"testing"
+)
+
+var regexpStr string = `^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,9}$`
+
+func BenchmarkMatchString(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		_, err := regexp.MatchString(regexpStr, "john.doe@example.tld")
+		if err != nil {
+			panic(err)
+		}
+	}
+}
+
+func BenchmarkMatchStringCompiled(b *testing.B) {
+	r, err := regexp.Compile(regexpStr)
+	if err != nil {
+		panic(err)
+	}
+
+	b.ResetTimer()
+	for n := 0; n < b.N; n++ {
+		r.MatchString("john.doe@example.tld")
+	}
+}
