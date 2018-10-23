61689c4e190014d56f304120b31509394b95daa4
Simon Waldherr <git@simon.waldherr.eu>
2018-10-24 01:57:18 +0200

    add random benchmark

---
+++ random/random_test.go
@@ -0,0 +1,48 @@
+package random
+
+import (
+	crand "crypto/rand"
+	"encoding/base64"
+	"math/big"
+	mrand "math/rand"
+	"testing"
+)
+
+func BenchmarkMathRand(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		mrand.Int63n(0xFFFF)
+	}
+}
+
+func BenchmarkCryptoRand(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		_, err := crand.Int(crand.Reader, big.NewInt(0xFFFF))
+		if err != nil {
+			panic(err)
+		}
+	}
+}
+
+func BenchmarkCryptoRandString(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		_, err := GenerateRandomString(32)
+		if err != nil {
+			panic(err)
+		}
+	}
+}
+
+func GenerateRandomBytes(n int) ([]byte, error) {
+	b := make([]byte, n)
+	_, err := mrand.Read(b)
+	if err != nil {
+		return nil, err
+	}
+
+	return b, nil
+}
+
+func GenerateRandomString(s int) (string, error) {
+	b, err := GenerateRandomBytes(s)
+	return base64.URLEncoding.EncodeToString(b), err
+}
