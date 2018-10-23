d98a21b1db5795683a319a863afe27134ad72887
Simon Waldherr <git@simon.waldherr.eu>
2018-10-23 23:55:44 +0200

    add parse benchmark

---
+++ parse/parse_test.go
@@ -0,0 +1,33 @@
+package parse
+
+import (
+	"strconv"
+	"testing"
+)
+
+func BenchmarkParseBool(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		_, err := strconv.ParseBool("true")
+		if err != nil {
+			panic(err)
+		}
+	}
+}
+
+func BenchmarkParseInt(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		_, err := strconv.ParseInt("1337", 10, 64)
+		if err != nil {
+			panic(err)
+		}
+	}
+}
+
+func BenchmarkParseFloat(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		_, err := strconv.ParseFloat("3.141592653589793238462643383", 64)
+		if err != nil {
+			panic(err)
+		}
+	}
+}
