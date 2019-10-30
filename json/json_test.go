5f5cae46cf73538a1d1acb4736512659e79bffa5
Yusuf Syaifudin <yusuf.syaifudin@gmail.com>
2019-10-30 16:52:42 +0700

    adding benchmark for json marshal and unmarshal using standar library

---
+++ json/json_test.go
@@ -0,0 +1,79 @@
+package json
+
+import (
+	"encoding/json"
+	"math"
+	"math/big"
+	"testing"
+	"time"
+)
+
+type Data struct {
+	String   string
+	Time     time.Time
+	Int      int
+	Int8     int8
+	Int16    int16
+	Int32    int32
+	Int64    int64
+	Boolean  bool
+	Float32  float32
+	Float64  float64
+	BigInt   big.Int
+	BigFloat big.Float
+}
+
+func BenchmarkJsonMarshal(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		var d = Data{
+			String:   "",
+			Time:     time.Now(),
+			Int:      math.MaxInt32,
+			Int8:     math.MaxInt8,
+			Int16:    math.MaxInt16,
+			Int32:    math.MaxInt32,
+			Int64:    math.MaxInt64,
+			Boolean:  false,
+			Float32:  math.MaxFloat32,
+			Float64:  math.MaxFloat64,
+			BigInt:   *big.NewInt(math.MaxInt64),
+			BigFloat: *big.NewFloat(math.MaxFloat64),
+		}
+
+		_, err := json.Marshal(d)
+		if err != nil {
+			b.Error(err)
+			b.Fail()
+			return
+		}
+	}
+}
+
+func BenchmarkJsonUnmarshal(b *testing.B) {
+	str := `
+{
+  "String": "",
+  "Time": "2019-10-30T16:41:29.853426+07:00",
+  "Int": 2147483647,
+  "Int8": 127,
+  "Int16": 32767,
+  "Int32": 2147483647,
+  "Int64": 9223372036854775807,
+  "Boolean": false,
+  "Float32": 3.4028235e+38,
+  "Float64": 1.7976931348623157e+308,
+  "BigInt": 9999999999999999999,
+  "BigFloat": "2.7976931348623157e+308"
+}
+`
+
+	for n := 0; n < b.N; n++ {
+		var d Data
+		err := json.Unmarshal([]byte(str), &d)
+		if err != nil {
+			b.Error(err)
+			b.Fail()
+			return
+		}
+	}
+}
